88c766a7dcb102b62be19ec61177384d7d91fa9c
Jake Weir <jweir136@yahoo.com>
2019-01-06 08:44:38 -0800

    first commit

---
+++ form/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+global home_link
+home_link = "https://www.tutorialspoint.com/django/django_page_redirection.htm"
+
+# Create your views here.
+def index(requests):
+	if requests.method == "POST":
+		name = requests.POST.get("name", False)
+		email = requests.POST.get("email", False)
+		if name and email:
+			user = User.objects.create_user(username=name, email=email)
+			return redirect(home_link)
+		
+	return render(requests, "form/index.html")

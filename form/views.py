cf9b54ef8c0cc8bfb3e3dc299c9f14c6ba7ff677
Jake Weir <jweir136@yahoo.com>
2019-01-06 08:58:58 -0800

    fix link

--- form/views.py
+++ form/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 global home_link
-home_link = "https://www.tutorialspoint.com/django/django_page_redirection.htm"
+home_link = "https://quant-qb43myb7x.now.sh"
 
 # Create your views here.
 def index(requests):
